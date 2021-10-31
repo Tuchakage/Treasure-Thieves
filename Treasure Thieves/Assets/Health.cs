8d4f3127dd7b88115130e530697f5ad729635f72
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-10-31 20:04:08 +0000

    Made Adjustments To The PVP Combat

--- Treasure Thieves/Assets/Health.cs
+++ Treasure Thieves/Assets/Health.cs
@@ -6,16 +6,26 @@ using Photon.Pun;
 public class Health : MonoBehaviourPun
 {
     public float health = 100;
+
+    Spellcaster spell;
+
+    [SerializeField]
+    public string attackname; // This will make it so that the game can find the name of the attack and find the amount of damage it does
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Gets The Spellcaster script
+        spell = GetComponent<Spellcaster>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // When the health is less than or equal to 0 then destroy the Player Game Object
+        if (health <= 0) 
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     [PunRPC]
@@ -26,6 +36,28 @@ public class Health : MonoBehaviourPun
         {
             health -= damagetaken;
             Debug.Log("Lost Health");
+        }
+
+    }
+
+    private void OnParticleCollision(GameObject col)
+    {
+        //Makes sure that when you get hit its from someone else and not yourself
+        if (!photonView.IsMine) 
+        {
+            //Depending on the attack the player will lose a certain amount of health
+            if (col.gameObject.tag == "Basic Attack" && this.gameObject.tag == "Player")
+            {
+                // This is here so the game can find the amount of damage this attack does
+                attackname = "Basic Attack";
+                //Gets the Photon View of the object it collided with
+                PhotonView photonView = PhotonView.Get(this);
+                //Gets the TakeDamage() function and applys it to the target
+                photonView.RPC("TakeDamage", RpcTarget.All, spell.DealDamage());
+                //Destroy The Spell Game Object
+                Destroy(col.transform.parent.gameObject);
+                //Debug.Log("Basic Attack has hit " + col.gameObject.name);
+            }
         }
 
     }
--- Treasure Thieves/Assets/Spellcaster.cs
+++ Treasure Thieves/Assets/Spellcaster.cs
@@ -5,33 +5,68 @@ using Photon.Pun;
 
 public class Spellcaster : MonoBehaviourPun
 {
+    [SerializeField]
+    private Health hs;
 
-    public GameObject projectile;
+    public GameObject basicattack;
+    public float dmg;
+
+    //Timer for the cool down
+    public float timer;
+    //The max cooldown value (Where the timer will start counting down from
+    public float cooldown;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Find The Health Script
+        hs = GetComponent<Health>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer > 0)
+        {
+            //When the cooldown for basic attack is more than 1 then it will start counting down
+            timer -= Time.deltaTime;
+        }
+
         //Makes sure i am controlling my own player
         if (photonView.IsMine) 
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                //Spawn in the fireball gameobject
-                GameObject lightning = Instantiate(projectile, transform) as GameObject;
-                //Make sure its not a child of the Player Game Object
-                lightning.transform.parent = null;
-                //Get the rigidbody of the spell game object that was just spawned
-                Rigidbody rb = lightning.GetComponent<Rigidbody>();
-                //Shoot the spell forward
-                rb.velocity = transform.forward * 20;
+                //If there is no cooldown (Its at 0) then player can use the basic attack
+                if (timer <= 0) 
+                {
+                    //Spawn in the fireball gameobject
+                    GameObject lightning = Instantiate(basicattack, transform) as GameObject;
+                    //Make sure its not a child of the Player Game Object
+                    lightning.transform.parent = null;
+                    //Get the rigidbody of the spell game object that was just spawned
+                    Rigidbody rb = lightning.GetComponent<Rigidbody>();
+                    //Shoot the spell forward
+                    rb.velocity = transform.forward * 20;
+                    cooldown = 1.0f;
+                    //Add cooldown to the basic attack
+                    timer = cooldown;
+                }
+
             }
         }
 
+    }
+
+    //This will send the Damage value of the attack to the Health Script
+    public float DealDamage()
+    {
+        //Depending on the spell game object depends on the amount of damage the attack will do
+        if (hs.attackname == "Basic Attack")
+        {
+            dmg = 5;
+        }
+
+        return dmg;
     }
 
 }

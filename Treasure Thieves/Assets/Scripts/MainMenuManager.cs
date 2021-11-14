fc2597d0b6687733ebdf9a874cc098661dc63196
Tuchakage <62402636+Tuchakage@users.noreply.github.com>
2021-11-14 00:18:03 +0000

    Revamped Matchmaking

---
+++ Treasure Thieves/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MainMenuManager : MonoBehaviourPunCallbacks, IPunObservable
+{
+    //Max Players in a room
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    [SerializeField]
+    private InputField playerName;
+
+    [SerializeField]
+    private Button joinButton;
+
+    [SerializeField]
+    private Text players, timertxt;
+
+    [SerializeField]
+    private float timer = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //If not connected to the Photon Network then connect
+        if (!PhotonNetwork.IsConnected)
+            PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If we are in a room
+        if (PhotonNetwork.InRoom) 
+        {
+            players.text = "Players: " + PhotonNetwork.CurrentRoom.PlayerCount + " of " + PhotonNetwork.CurrentRoom.MaxPlayers;
+
+            //Lists all the players in the room
+            players.text += ":\n";
+            Dictionary<int, Player> mydict = PhotonNetwork.CurrentRoom.Players;
+            int i = 1;
+            //Checks the names of each player
+            foreach (var item in mydict)
+                players.text += string.Format("{0,2}. {1}\n", (i++), item.Value.NickName);
+
+            DisplayTime(timer);
+
+            //If the timer variable is more than 0 
+            if (timer > 0)
+            {
+                // Then Timer will start counting down
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                //When the timer reaches 0 the game will start
+                StartGame();
+            }
+
+        }
+            
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //Once connected to Photon server...
+        Debug.Log("OnConnectedToMaster was called by PUN.");
+        //Allows us to create and join rooms
+        PhotonNetwork.JoinLobby();
+
+        //Gets the Players name and sets the text to the player name
+        //playerName.text = PlayerPrefs.GetString("PlayerName");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //When we have joined a lobby 
+
+
+    }
+
+    //When you press the JoinRoom button
+    public void JoinRoom()
+    {
+
+        //Sets the player name (Player name is set to PlayerPrefs)
+        PlayerPrefs.SetString("PlayerName", playerName.text);
+        PhotonNetwork.NickName = playerName.text;
+        //Joins a random room
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        //Disable Join Button
+        joinButton.gameObject.SetActive(false);
+        //Make it so you can no longer input a name
+        playerName.gameObject.SetActive(false);
+        //Show the list of Players in the room
+        players.gameObject.SetActive(true);
+        //Show the Timer
+        timertxt.gameObject.SetActive(true);
+    }
+
+    //Everytime A Player joins the room
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
+    {
+        //Check if the room is full 
+        if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers)
+        {
+            //If room is full then make the timer to be 5 seconds
+            timer = 5;
+        }
+    }
+
+    //Everytime a player leaves the room
+    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
+    {
+        //Check if the room is not full 
+        if (PhotonNetwork.CurrentRoom.PlayerCount == PhotonNetwork.CurrentRoom.MaxPlayers - 1)
+        {
+            //If room is not full then reset the timer
+            timer = 60;
+        }
+    }
+
+    public void StartGame() 
+    {
+        //Go To The Actual Game
+        PhotonNetwork.LoadLevel("Battle");
+
+        //Make sure no one can join whilst game is going on
+        PhotonNetwork.CurrentRoom.IsVisible = false;
+        PhotonNetwork.CurrentRoom.IsOpen = false;
+    }
+
+    void DisplayTime(float time)
+    {
+        //This calculates the minutes. Mathf.FloorToInt rounds them down to the largest integer value
+        float minutes = Mathf.FloorToInt(time / 60);
+
+        //This calculates the seconds e.g 62 % 60 would return the value 2 aka 1 minute and 2 seconds. 
+        float seconds = Mathf.FloorToInt(time % 60);
+
+        //string.Format allows us to place variables inside of a formatted string
+        timertxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Oops, tried to join a room and failed. Calling CreateRoom!");
+
+        // failed to join a random room, so create a new one
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    //This function allows the variables inside to be sent over the network
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            //We own this player so send the other computers the data
+            stream.SendNext(timer);
+
+        }
+        else
+        {
+            //Network player that receives the data
+            timer = (float)stream.ReceiveNext();
+
+        }
+    }
+}
--- Treasure Thieves/Assets/Scripts/NetworkManager.cs
+++ Treasure Thieves/Assets/Scripts/NetworkManager.cs
@@ -10,25 +10,20 @@ using ExitGames.Client.Photon;
 //This script inherits from MonoBehaviourPunCallbacks instead of just MonoBehaviour
 public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
 {
-    //Max Players in a room
-    [SerializeField]
-    private byte maxPlayersPerRoom = 20;
+
 
     //Variables to connect our Network Manager to the UI elements
     [SerializeField]
     private Text nickname, status, room, players, bluescoretext, redscoretext, bluewinnertext, redwinnertext;
     [SerializeField]
-    private Button buttonPlay, buttonLeave, buttonRespawn;
-    [SerializeField]
-    private InputField playerName;
+    private Button buttonLeave, buttonRespawn;
     [SerializeField]
     private Button blue_SpellClass, blue_WarriorClass, red_SpellClass, red_WarriorClass, redTeam, blueTeam; //Team Buttons
-    [SerializeField]
-    public int bluescore, redscore; //Used for the Scores
-
+    public int bluescore, redscore, bluePlayerCount, redPlayerCount, maxInTeam;
     public int teamPick = 0;
     public bool win = false; // Used to identify if there is a winner
     // Game Object of player
+
     public GameObject player;
     //Checks if the player is alive
     public bool isAlive;
@@ -46,136 +41,64 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
     void Start()
     {
         teamPick = 0;
-        status.text = "Connecting...";
-        buttonPlay.gameObject.SetActive(false);
-        buttonLeave.gameObject.SetActive(false);
-        playerName.gameObject.SetActive(false);
-        buttonRespawn.gameObject.SetActive(false);
-
-
-        redTeam.gameObject.SetActive(false);
-        blueTeam.gameObject.SetActive(false);
-
-        blue_SpellClass.gameObject.SetActive(false);
-        blue_WarriorClass.gameObject.SetActive(false);
-        red_SpellClass.gameObject.SetActive(false);
-        red_WarriorClass.gameObject.SetActive(false);
         //Make sure the blue and red score is 0
         bluescore = 0;
         redscore = 0;
+        bluePlayerCount = 0;
+        redPlayerCount = 0;
         //Make sure the winner Text is empty
         bluewinnertext.text = " ";
         redwinnertext.text = " ";
-        //If not connected to the Photon Network then connect
-        if (!PhotonNetwork.IsConnected)
-            PhotonNetwork.ConnectUsingSettings();
+
+        //So the Respawn button doesnt show up when the game starts
+        isAlive = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (PhotonNetwork.InRoom)
+        //Keep updating the score
+        bluescoretext.text = "Blue Score: " + bluescore;
+        redscoretext.text = "Red Score: " + redscore;
+
+        //If the player health is 0
+        if (!isAlive)
         {
-            //Keep updating the score
-            bluescoretext.text = "Blue Score: " + bluescore;
-            redscoretext.text = "Red Score: " + redscore;
-            nickname.text = "Hello, " + PhotonNetwork.NickName;
-            room.text = "Room: " + PhotonNetwork.CurrentRoom.Name;
-            players.text = "Players: " + PhotonNetwork.CurrentRoom.PlayerCount + " of " + PhotonNetwork.CurrentRoom.MaxPlayers;
-            //Lists all the players in the room
-            players.text += ":\n";
-            Dictionary<int, Player> mydict = PhotonNetwork.CurrentRoom.Players;
-            int i = 1;
-            //Checks the names of each player
-            foreach (var item in mydict)
-                players.text += string.Format("{0,2}. {1}\n", (i++), item.Value.NickName);
-
-            if (!isAlive)
+            buttonRespawn.gameObject.SetActive(true);
+            Cursor.visible = true;
+            //Respawn Option to change classes
+            if (teamPick == 1)
             {
-                buttonRespawn.gameObject.SetActive(true);
-
-                //Respawn Option to change classes
-                if (teamPick == 1)
-                {
-                    blue_SpellClass.gameObject.SetActive(true);
-                    blue_WarriorClass.gameObject.SetActive(true);
-                    red_SpellClass.gameObject.SetActive(false);
-                    red_WarriorClass.gameObject.SetActive(false);
-                }
-                else if (teamPick == 2)
-                {
-                    red_SpellClass.gameObject.SetActive(true);
-                    red_WarriorClass.gameObject.SetActive(true);
-                    blue_SpellClass.gameObject.SetActive(false);
-                    blue_WarriorClass.gameObject.SetActive(false);
-                }
-
+                blue_SpellClass.gameObject.SetActive(true);
+                blue_WarriorClass.gameObject.SetActive(true);
+                red_SpellClass.gameObject.SetActive(false);
+                red_WarriorClass.gameObject.SetActive(false);
             }
-
-            //Keep checking the score
-            CheckScore();
-            //If there is a winner then End the game
-            if (win)
+            else if (teamPick == 2)
             {
-                EndGame();
+                red_SpellClass.gameObject.SetActive(true);
+                red_WarriorClass.gameObject.SetActive(true);
+                blue_SpellClass.gameObject.SetActive(false);
+                blue_WarriorClass.gameObject.SetActive(false);
             }
 
         }
-        else if (PhotonNetwork.IsConnected)
+
+        //Keep checking the score
+        CheckScore();
+        //If there is a winner then End the game
+        if (win)
         {
-            nickname.text = "Type your name below and hit PLAY!";
-            room.text = "Not yet in a room...";
-            players.text = "Players: 0";
+            EndGame();
         }
-        else
-            nickname.text = room.text = players.text = "";
 
-    }
-    //When it has connected to Photon servers
-    public override void OnConnectedToMaster()
-    {
-        //Once connected to Photon server...
-        Debug.Log("OnConnectedToMaster was called by PUN.");
-        status.text = "Connected to Photon.";
-
-
-        //Do not show the Play button
-        buttonPlay.gameObject.SetActive(false);
-        //Show the box where players can write their own names
-        playerName.gameObject.SetActive(true);
-        //Makes sure the leave button doesnt show
-        buttonLeave.gameObject.SetActive(false);
-        //Make sure the Respawn button doesnt show
-        buttonRespawn.gameObject.SetActive(false);
-
-        //Make sure player can pick a team
-        redTeam.gameObject.SetActive(true);
-        blueTeam.gameObject.SetActive(true);
-        blue_SpellClass.gameObject.SetActive(false);
-        blue_WarriorClass.gameObject.SetActive(false);
-        red_SpellClass.gameObject.SetActive(false);
-        red_WarriorClass.gameObject.SetActive(false);
-
-        //Gets the Players name and sets the text to the player name
-        playerName.text = PlayerPrefs.GetString("PlayerName");
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("Oops, tried to join a room and failed. Calling CreateRoom!");
-
-        // failed to join a random room, so create a new one
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
-    }
-
-    //When you press the Play button
-    public void Play()
-    {
-        //Sets the player name (Player name is set to PlayerPrefs)
-        PlayerPrefs.SetString("PlayerName", playerName.text);
-        PhotonNetwork.NickName = playerName.text;
-        //Joins a random room
-        PhotonNetwork.JoinRandomRoom();
+        //MAKE SURE TO DELETE LATER
+        if (Input.GetKeyDown(KeyCode.H)) 
+        {
+            bluePlayerCount++;
+            redPlayerCount++;
+        }
     }
 
     //When you press the Leave button
@@ -183,49 +106,6 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
     {
         //Leaves the room you are in
         PhotonNetwork.LeaveRoom();
-    }
-
-    //When you have joined a room the following code will run
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("Yep, you managed to join a room!");
-        status.text = "Yep, you managed to join a room!";
-        //Disable THe Play button
-        buttonPlay.gameObject.SetActive(false);
-        //Disable The option to type in your name
-        playerName.gameObject.SetActive(false);
-        //Shows the leave button
-        buttonLeave.gameObject.SetActive(true);
-
-        redTeam.gameObject.SetActive(false);
-        blueTeam.gameObject.SetActive(false);
-
-
-        //Disable Class Changing Buttons
-        blue_SpellClass.gameObject.SetActive(false);
-        blue_WarriorClass.gameObject.SetActive(false);
-        red_SpellClass.gameObject.SetActive(false);
-        red_WarriorClass.gameObject.SetActive(false);
-
-        //Spawn Players
-        PhotonNetwork.Instantiate(player.name,
-         new Vector3(spawnLocation.x, 1, spawnLocation.z),
-        Quaternion.Euler(0, spawnRotation.y, 0)
-        , 0);
-  
-        //Player Will Be Alive
-        isAlive = true;
-
-    }
-
-    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer)
-    {
-        status.text = newPlayer.NickName + " has just entered.";
-    }
-
-    public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer)
-    {
-        status.text = otherPlayer.NickName + " has just left.";
     }
 
     public override void OnLeftRoom()
@@ -274,38 +154,52 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
 
     public void blue_Team_Pick()
     {
+        //Dont do anything if the team is full
+        if (bluePlayerCount == maxInTeam)
+            return;
         //Choose Blue Team
         teamPick = 1;
+        //Increase the amount of players in the team
+        photonView.RPC("IncreaseBluePlayerCount", RpcTarget.All);
         //Default Pick The Spell Caster Character
         player = bp_SC_Prefab;
         //Debug.Log("I am blue team");
-        //Reactivate the play so the player can play the game
-        buttonPlay.gameObject.SetActive(true);
         //Blue Team Active Buttons 
         blue_SpellClass.gameObject.SetActive(true);
         blue_WarriorClass.gameObject.SetActive(true);
         red_SpellClass.gameObject.SetActive(false);
         red_WarriorClass.gameObject.SetActive(false);
+        //Disable Team Picking
+        redTeam.gameObject.SetActive(false);
+        blueTeam.gameObject.SetActive(false);
         //Spawn Location For Blue Team
         spawnLocation = new Vector3(88.551f, 1.07f, 5.1f);
         //Spawn Rotation For Blue Team
         spawnRotation = Quaternion.Euler(0, -90, 0);
+
     }
 
     public void red_Team_Pick()
     {
+        //Dont do anything if the team is full
+        if (redPlayerCount == maxInTeam)
+            return;
+
         //Choose Red Team
         teamPick = 2;
+        //Increase the amount of players in the team
+        photonView.RPC("IncreaseRedPlayerCount", RpcTarget.All);
         //Default Pick The Spell Caster Character
         player = rp_SC_Prefab;
-        //Reactivate the play so the player can play the game
-        buttonPlay.gameObject.SetActive(true);
         //Debug.Log("I am red team");
         //Red Team Active Buttons 
         red_SpellClass.gameObject.SetActive(true);
         red_WarriorClass.gameObject.SetActive(true);
         blue_SpellClass.gameObject.SetActive(false);
         blue_WarriorClass.gameObject.SetActive(false);
+        //Disable Team Picking
+        redTeam.gameObject.SetActive(false);
+        blueTeam.gameObject.SetActive(false);
         //Spawn Location For Red Team
         spawnLocation = new Vector3(-58.29f, 1.07f, 5.1f);
         //Spawn Rotation For Red Team
@@ -316,12 +210,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
     {
         //Change player prefab to spellcaster
         player = bp_SC_Prefab;
+        Respawn();
         //Debug.Log("Blue Team: Class type changed to Spellcaster");
     }
     public void pick_BP_Warrior_Class()
     {
         //Change player prefab to warrior
         player = bp_WR_Prefab;
+        Respawn();
         //Debug.Log("Class type changed to warrior");
     }
 
@@ -329,12 +225,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
     {
         //Change player prefab to spellcaster
         player = rp_SC_Prefab;
+        Respawn();
         //Debug.Log("Class type changed to spellcaster");
     }
     public void pick_RP_Warrior_Class()
     {
         //Change player prefab to warrior
         player = rp_WR_Prefab;
+        Respawn();
         //Debug.Log("Class type changed to warrior");
     }
 
@@ -356,6 +254,18 @@ public class NetworkManager : MonoBehaviourPunCallbacks, IPunObservable
         blue_WarriorClass.gameObject.SetActive(false);
         red_SpellClass.gameObject.SetActive(false);
         red_WarriorClass.gameObject.SetActive(false);
+    }
+
+    [PunRPC]
+    void IncreaseBluePlayerCount() 
+    {
+        bluePlayerCount++;
+    }
+
+    [PunRPC]
+    void IncreaseRedPlayerCount()
+    {
+        redPlayerCount++;
     }
 
     //This function allows the variables inside to be sent over the network
